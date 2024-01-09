a7a1ca7e4c08226b047eae4073495e1394ab930b
kuznimyk <kuznimyk@gmail.com>
2024-01-09 09:37:56 -0700

    aws rds database connection

--- prestigueclothing/settings.py
+++ prestigueclothing/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'prestigueclothing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'maindb',
+        'USER':'postgres',
+        'PASSWORD':'maddrip4ever',
+        'HOST':'databasemain.cb60gmq862t9.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
